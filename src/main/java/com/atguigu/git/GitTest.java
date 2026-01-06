f7493bf68b5ebfece5e491b72db3040735c58666
dongchen <dczhang123@hotmail.com>
2026-01-05 23:28:17 -0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Dongchen Zhang
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
